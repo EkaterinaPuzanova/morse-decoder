e1f9469977316178d9a6150de44e7becc55021db
katya_puzan <katesamek357@gmail.com>
2022-01-31 23:38:56 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    MORSE_TABLE[' '] = ' ';
+
+    let arrExpr = expr.split('');
+    let result1 = [];
+    while(arrExpr.length) result1.push(arrExpr.splice(0,2).reduce(function(sum, elem) {return sum + elem}));
+    let result2 = [];
+    while(result1.length) result2.push(result1.splice(0,5));
+   
+    for (let i = 0; i < result2.length; i++) {
+      result2[i] = result2[i].filter(x => x != '00');  
+    }
+  
+    for (let f = 0; f < result2.length; f++) {
+      for (let g = 0; g < result2[f].length; g++) {
+        if (result2[f][g] === '10') {result2[f][g] = '.'};
+        if (result2[f][g] === '11') {result2[f][g] = '-'};
+        if (result2[f][g] === '**') {result2[f] = [' ']};
+      }
+    }
+   
+    for (let k = 0; k < result2.length; k++) {
+       result2[k]=result2[k].reduce(function(sum, elem) {return sum + elem}, '');
+       result2[k] = MORSE_TABLE[result2[k]];
+    }
+   
+    return result2.reduce(function(sum, elem) {return sum + elem}, '');  
+   
 }
 
 module.exports = {
